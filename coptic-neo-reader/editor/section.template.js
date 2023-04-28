c48a9ec495c6504643977c76589a0845dbde3417
Muhammad Mohsen <muhammadaeh@gmail.com>
2023-04-29 00:25:54 +0200

    Optimized editor assets + Modified editor styling

--- coptic-neo-reader/editor/section.template.js
+++ coptic-neo-reader/editor/section.template.js
@@ -1,53 +1,51 @@
-const sectionTemplate = `
-	<section>
-		<div class="inline equal">
-			<div class="input margin">
-				<!-- TYPE -->
-				<select data-attr="type">
-					<option value="title">Title</option>
-					<option value="else">Something Else</option>
-				</select>
-				<label>Type</label>
-			</div>
-
-			<!-- ROLE -->
-			<div class="input margin">
-				<select data-attr="role">
-					<option value="any">Any</option>
-					<option value="deacon">Deacon</option>
-					<option value="role3">Role 3</option>
-				</select>
-				<label>Role</label>
-			</div>
-
-			<!-- DATE -->
-			<div class="input margin">
-				<input type="text" placeholder="material" data-attr="date">
-				<label>Date Range (Optional)</label>
-			</div>
-
-			<button class="icon close secondary" onclick="ContentEditor.remove(this)">
-				<img src="assets/ic_close.svg" alt="Remove Section">
-			</button>
-			<button class="icon drag secondary" onclick="ContentEditor.drag(this)">
-				<img src="assets/ic_drag.svg" alt="Drag/Drop">
-			</button>
+const sectionTemplate = `<section>
+	<div class="inline equal">
+		<div class="input margin">
+			<!-- TYPE -->
+			<select data-attr="type">
+				<option value="title">Title</option>
+				<option value="else">Something Else</option>
+			</select>
+			<label>Type</label>
 		</div>
 
-		<div class="input content">
-			<textarea data-attr="en" placeholder="material"></textarea>
-			<label>English</label>
+		<!-- ROLE -->
+		<div class="input margin">
+			<select data-attr="role">
+				<option value="any">Any</option>
+				<option value="deacon">Deacon</option>
+				<option value="role3">Role 3</option>
+			</select>
+			<label>Role</label>
 		</div>
 
-		<div class="input content">
-			<textarea data-attr="ar" placeholder="material"></textarea>
-			<label>Arabic</label>
+		<!-- DATE -->
+		<div class="input margin">
+			<input type="text" placeholder="material" data-attr="date">
+			<label>Date Range (Optional)</label>
 		</div>
 
-		<div class="input content">
-			<textarea data-attr="cp" placeholder="material"></textarea>
-			<label>Coptic</label>
-		</div>
+		<button class="icon close secondary" onclick="ContentEditor.remove(this)">
+			<img src="assets/ic_close.svg" alt="Remove Section">
+		</button>
+		<button class="icon drag secondary" onclick="ContentEditor.drag(this)">
+			<img src="assets/ic_drag.svg" alt="Drag/Drop">
+		</button>
+	</div>
 
-	</section>
-`;
+	<div class="input content">
+		<textarea data-attr="en" placeholder="material"></textarea>
+		<label>English</label>
+	</div>
+
+	<div class="input content">
+		<textarea data-attr="ar" placeholder="material"></textarea>
+		<label>Arabic</label>
+	</div>
+
+	<div class="input content">
+		<textarea data-attr="cp" placeholder="material"></textarea>
+		<label>Coptic</label>
+	</div>
+
+</section>`;
