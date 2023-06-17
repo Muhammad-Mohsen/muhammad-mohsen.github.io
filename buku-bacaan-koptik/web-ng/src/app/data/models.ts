4476f262bc397607dd3048ec2e4e3e2342f65fa3
Muhammad Mohsen <muhammad.mohsen@innuva.com>
2023-06-17 06:39:14 +0300

    Removed unused "web-ng" folder

--- buku-bacaan-koptik/web-ng/src/app/data/models.ts
+++
@@ -1,9 +0,0 @@
-export class MenuItem {
-	constructor(
-		readonly text: String,
-		readonly image?: String,
-		readonly constraints?: any[],
-		readonly menu?: MenuItem[],
-		readonly path?: String
-	) {}
-}
