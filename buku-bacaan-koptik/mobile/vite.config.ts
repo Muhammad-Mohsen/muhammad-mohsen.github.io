5e89d2c0afa492d7322cd0924352913ed0cded39
Muhammad Mohsen <muhammadaeh@gmail.com>
2023-06-29 15:51:40 +0300

    Added vite-static-copy plugin to force-copy non-referenced assets

--- buku-bacaan-koptik/mobile/vite.config.ts
+++ buku-bacaan-koptik/mobile/vite.config.ts
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, normalizePath } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'node:path'
 
 export default defineConfig({
   root: '../web',
-  // publicDir: '../web/assets',
   build: {
     outDir: '../mobile/dist',
     minify: false,
     emptyOutDir: true,
   },
+
+  // publicDir: '../web/assets', // doesn't work because it breaks the non-built version
+  plugins: [
+
+    viteStaticCopy({
+      targets: [
+        {
+          src: normalizePath(path.resolve(__dirname, '../web/assets/documents')) + '/[!.]*', // copy documents to build
+          dest: 'assets/documents',
+        },
+        {
+          src: normalizePath(path.resolve(__dirname, '../web/assets/images')) + '/[!.]*', // copy images to build
+          dest: 'assets/images',
+        },
+      ],
+    }),
+
+  ],
 });
